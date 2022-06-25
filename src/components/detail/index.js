6e70b27be4af5c9b222d4e37fd9607a7741034b4
Zehra Baran <brnzehraa@gmail.com>
2022-06-25 23:16:08 +0300

    Editing has been done in detail and added detail images

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -3,62 +3,70 @@ import { Col, Row } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react"
 import Header from "../header";
+import { options } from '../../constant'
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
 
-    const [exercises, setExercises] = useState({})
+  const [exercises, setExercises] = useState({})
 
-    const getExercisesById = () => {
-       
-            const options = {
-              method: 'GET',
-              url: 'https://exercisedb.p.rapidapi.com/exercise/',
-              headers: {
-                'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-              }
-            };
-      
-            axios.request(options).then(function (response) {
-              setExercises(response.data)
-              console.log(response.data);
-            }).catch(function (error) {
-              console.error(error);
-            });
-      
-          
+  let { id } = useParams();
 
+  console.log('id', id)
+
+  const getExercisesById = () => {
+    options.url = `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`
+    axios.request(options).then(function (response) {
+      setExercises(response.data)
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+
+  }
+
+  useEffect(() => {
+    if (Object.keys(exercises).length === 0 && !!id) {
+      getExercisesById()
     }
+  })
 
-    useEffect(() => {
-        if (Object.keys(exercises).length === 0) {
-            getExercisesById()
-        }
-      })
+  return (
+    <>
+      <div>
+        <Header />
+        <Row>
+          <div className="flex w-full">
 
-    return(
-        <>
-        <div>
-            <Header/>
-            <Row>
-                <Col span={12}>
-                    
-                    
-                    
-                </Col>
-                <Col span={12}>
-                    <div>
-                    <h3>deneme{exercises.name}</h3>
+          
+          <Col span={12}>
+              <img src={exercises.gifUrl} alt="Gif" className="w-[730px] h-[750px]"/>
+          </Col>
+          <Col span={12}>
+            <div>
+              <span className="text-5xl font-bold">{exercises.name}</span>
+            </div>
+            <div className="flex flex-col">
+              <div>
+              <span>{exercises.bodyPart}</span>
 
-                    </div>
-                    
-                </Col>
-            </Row>
-        </div>
+              </div>
+              
+              <span>{exercises.target}</span>
+              <span>{exercises.equipment}</span>
+              
+            </div>
+          </Col>
+          </div>
 
-      
-        </>
-    )
+        </Row>
+      </div>
+
+
+    </>
+  )
 }
 
 export default Detail
